97276bda9c82e671df44a785c9b066dee1fb73a3
Alessandrobau <alessandrobau011@gmail.com>
2023-11-19 10:35:59 -0300

    update

--- testte.py
+++ testte.py
@@ -1,243 +1,32 @@
-# nodo
-class Node:
-    def __init__(self, key):
-        self.key = key
-        self.right = None
-        self.left = None
-        self.height = 1
+import random
 
+class Jogo:
+    def __init__(self, jogo_id, titulo, desenvolvedor, preco, generos):
+        self.jogo_id = jogo_id
+        self.titulo = titulo
+        self.desenvolvedor = desenvolvedor
+        self.preco = preco
+        self.generos = generos
 
-# arvore AVL
-class AVLtree:
-    def __init__(self):
-        self.base = None
+def gerar_jogos(num_jogos):
+    jogos = {}
 
-    # função que obtém altura do nó
-    def _height(self, node):
-        if node is None:
-            return 0
-        return node.height
+    for i in range(num_jogos):
+        jogo_id = i + 1  # IDs começando em 1
+        titulo = f"Jogo {jogo_id}"
+        desenvolvedor = f"Desenvolvedor {jogo_id}"
+        preco = round(random.uniform(10, 200), 2)  # Preço aleatório entre 10 e 60
+        generos = random.sample(["Ação", "Aventura", "RPG", "Esportes"], random.randint(1, 3))
 
-    # calcula o nivel de balanceamento do nó
-    def _balance(self, node):
-        if node is None:
-            return 0
-        return self._height(node.left) - self._height(node.right)
+        jogo = Jogo(jogo_id, titulo, desenvolvedor, preco, generos)
+        jogos[jogo_id] = jogo
 
-    # rotação simples para a direita
-    def _rightSimpleRotation(self, y):  # recebe um nó e armazena no y
-        x = y.left  # atribui o filho esquerdo de y ao x
-        Temp = x.right  # armazena o filho direito de x na variavel temp
-        # faz a atualização
-        x.right = y  # y se torna o filho direito de x
-        y.left = Temp  # filho esquerdo de y se torna temp
+    return jogos
 
-        y.height = 1 + max(
-            self._height(y.left), self._height(y.right)
-        )  # atualiza a altura do nó/ faz o calculo da altura com base na altura maxima dos filhos esquerdo e direito e adiciona 1
-        x.height = 1 + max(self._height(x.left), self._height(x.right))
+# Exemplo de uso
+num_jogos = 100
+jogos_aleatorios = gerar_jogos(num_jogos)
 
-        return x  # retorna x que é a nova raiz da subarvore
-
-    # rotação simples para a esquerda
-    def _leftSimpleRotation(self, y):
-        x = y.right  # atribui o filho direito de y ao x
-        temp = x.left  # atribui o filho esquerdo de x ao temp
-        # atualização
-        x.left = y  # y se torna filho esquerdo de x
-        y.right = temp  # filho direito de y se torna temp
-
-        y.height = 1 + max(
-            self._height(y.left), self._height(y.right)
-        )  # atualiza a altura do nó/ faz o calculo da altura com base na altura maxima dos filhos esquerdo e direito e adiciona 1
-        x.height = 1 + max(self._height(x.left), self._height(x.right))
-
-        return x  # retorna x que é a nova raiz da subarvore
-
-    # inserir
-    def insertOnAVL(self, base, key):
-        if base is None:
-            return Node(key) 
-
-        if key < base.key:
-            base.left = self.insertOnAVL(
-                base.left, key
-            )  # inserido na esquerda se for menor
-        else:
-            base.right = self.insertOnAVL(base.right, key)  # na direita se for maior
-        base.height = 1 + max(self._height(base.left), self._height(base.right))
-
-        balance = self._balance(base)
-
-        # caso 1 - rotação simples a esquerda
-        if (
-            balance < -1 and key > base.right.key
-        ):  # se a balance estiver -1 e a chave for maior que o nodo na direita, faz uma rotação simples à esquerda
-            return self._leftSimpleRotation(base)
-        # caso 2 - rotação simples à direita
-        if (
-            balance > 1 and key < base.left.key
-        ):  # se a balance estiver 1 e a chave for menor que o nodo na esquerda, faz uma rotação simples à direita
-            return self._rightSimpleRotation(base)
-        # caso 3 - rotação dupla à esquerda-direita
-        if balance > 1 and key > base.left.key:
-            base.left = self._leftSimpleRotation(base.left)
-            return self._rightSimpleRotation(base)
-        # caso 4 - rotação dupla à direita-esquerda
-        if balance < -1 and key < base.right.key:
-            base.right = self._rightSimpleRotation(base.right)
-            return self._leftSimpleRotation(base)
-
-        return base
-
-    ################
-    # Função auxiliar para encontrar o nó mínimo na árvore
-    def _minimalValueNode(self, node):
-        current = node
-        while current.left is not None:
-            current = current.left
-        return current
-
-    # Função para remover um nó da árvore AVL
-    def deleteOnAVL(self, base, key):
-        if base is None:
-            return base
-
-        if key < base.key:
-            base.left = self.deleteOnAVL(base.left, key)
-        elif key > base.key:
-            base.right = self.deleteOnAVL(base.right, key)
-        else:
-            if base.left is None or base.right is None:
-                if base.left:
-                    temp = base.left
-                else:
-                    temp = base.right
-                if temp is None:
-                    temp = base
-                    base = None
-                else:
-                    base = temp
-                temp = None
-            else:
-                temp = self._minimalValueNode(base.right)
-                base.key = temp.key
-                base.right = self.deleteOnAVL(base.right, temp.key)
-
-        if base is None:
-            return base
-
-        base.height = 1 + max(self._height(base.left), self._height(base.right))
-        balance = self._balance(base)
-
-        # caso 1 - rotação simples a esquerda
-        if (
-            balance < -1 and key > base.right.key
-        ):  # se a balance estiver -1 e a chave for maior que o nodo na direita, faz uma rotação simples à esquerda
-            return self._leftSimpleRotation(base)
-        # caso 2 - rotação simples à direita
-        if (
-            balance > 1 and key < base.left.key
-        ):  # se a balance estiver 1 e a chave for menor que o nodo na esquerda, faz uma rotação simples à direita
-            return self._rightSimpleRotation(base)
-        # caso 3 - rotação dupla à esquerda-direita
-        if balance > 1 and key > base.left.key:
-            base.left = self._leftSimpleRotation(base.left)
-            return self._rightSimpleRotation(base)
-        # caso 4 - rotação dupla à direita-esquerda
-        if balance < -1 and key < base.right.key:
-            base.right = self._rightSimpleRotation(base.right)
-            return self._leftSimpleRotation(base)
-
-        return base
-
-    # Função para inserir um novo nó na árvore
-    def insertNode(self, key):
-        self.base = self.insertOnAVL(self.base, key)
-
-    # Função para remover um nó da árvore
-    def deleteNode(self, key):
-        self.base = self.deleteOnAVL(
-            self.base, key
-        )  # esconder do usuario funções recursivas
-
-    # Função para realizar uma travessia em ordem na árvore AVL
-    def inOrderTraversal(self, node):  # vendo onde termina o nodo
-        if node:  # checagem se não é nulo
-            self.inOrderTraversal(node.left)  #
-            print(
-                node.key, end=" "
-            )  # "end" por padrão o print poe nova linha end substitui por espaço
-            self.inOrderTraversal(node.right)
-
-    # Função para realizar uma travessia em ordem na árvore AVL
-    def inOrder(self):
-        self.inOrderTraversal(
-            self.base
-        )  # inOrderTraversal recebe node, usuario nao precisa saber quem é node
-
-    # Função de busca por valor especifico
-    def searchForItemTraversal(self, node, price):  # função recursiva
-        if node:
-            if node.key == price:
-                return node.key  
-            if price <= node.key:
-                return self.searchForItemTraversal(node.left, price)
-            elif price >= node.key:
-                return self.searchForItemTraversal(node.right, price)
-            else:
-                return -1  # preço impossivel
-
-    def searchForItem(self, price):
-        return self.searchForItemTraversal(self.base, price)
-    
-
-    def searchRangeTraversal(self, node, min, max, prices):
-        if node:
-            if prices != None:
-                self.searchRangeTraversal(node.left, min, max, prices)
-                prices.append(node.key)
-                self.searchRangeTraversal(node.right, min, max, prices)
-            else:
-                if min <= node.key and node.left != None:
-                    newList = self.searchRangeTraversal(node.left, min, max, prices)
-                    if newList != None:
-                        newList.append(node.key)
-                        newList.extend(self.searchRangeTraversal(node.right, min, max, newList) or [])
-                    return newList
-                elif min >= node.key:
-                    return self.searchRangeTraversal(node.right, min, max, prices)
-                else:
-                    return [node.key] # cria lista sem nome com o menor valor
-        else:
-            return prices
-
-    def searchRange(self, min, max):
-        return self.searchRangeTraversal(self.base, min, max, None)
-    
-    def printTree(self, node=None, level=0, prefix="Root: "):
-        if node is not None:
-            print(" " * (level * 4) + prefix + str(node.key))
-            if node.left is not None or node.right is not None:
-                if node.left:
-                    self.printTree(node.left, level + 1, "L--- ")
-                if node.right:
-                    self.printTree(node.right, level + 1, "R--- ")
-
-
-avltree = AVLtree()
-keys = [10, 40, 60, 20, 30, 100, 10, 20, 30, 6 , 12, 24, 45, 6, 6, 6, 6, 6, 102]
-for key in keys:
-    avltree.insertNode(key)
-
-# print("arvore inicial ")
-# avltree.inOrder()
-# print()
-
-# avltree.deleteNode(100)
-
-# print("arvore ")
-
-# avltree.inOrder()
-# print()
-# print(avltree.searchRange(5, 100))
+# Imprimir os jogos gerados
+for jogo_id, jogo in jogos_aleatorios.items():
+    print(f"Jogo ID: {jogo.jogo_id}, Título: {jogo.titulo}, Desenvolvedor: {jogo.desenvolvedor}, Preço: {jogo.preco}, Gêneros: {jogo.generos}")
--- trabalho.py
+++ trabalho.py
@@ -1,57 +1,86 @@
 from arvoreAVL import *
-# class HashGeneros:
-#     def __init__(self):
-#         self.genero_para_jogos = {}
-#     def adicionar_jogo(self, jogo):
-#         pass
-#     # Adicionar jogos na hash table de acordo com seu(s) gênero(s)
-#     def obter_jogos(self, genero):
-#         pass
-#     # Buscar jogos de um determinado gênero
-# ''''''
+import random
+class HashGeneros:
+    def __init__(self):
+        self.genero_para_jogos = {}
 
-######criar menu fdisk#######
+    def get_hash(self):
+        pass
+
+    def adicionar_jogo(self, jogo):
+        pass
+    def obter_jogos(self, genero):
+        pass
+
 class Jogo:
     def __init__(self, jogo_id, titulo, desenvolvedor, preco, generos):
         self.jogo_id = jogo_id
         self.titulo = titulo
         self.desenvolvedor = desenvolvedor
         self.preco = preco
-        self.generos = generos # Lista, pois um jogo pode pertencer a múltiplos gêneros
+        self.generos = generos 
 class NoJogo:
     def __init__(self, jogo):
         self.jogo = jogo
         self.esquerda = None
         self.direita = None
 
-class MotorBuscaJogos:
+class MotorBuscaJogos: ## verificar se é necessario
     def __init__(self):
         pass
         #self.catalogo_jogos = ArvoreJogos()
         # self.generos = HashGeneros()
     # ... Outros més para adicionar jogos, busca, etc
-    
+
+
+
+def gerar_jogos(num_jogos):
+        jogos = {}
+        for i in range(num_jogos):
+            jogo_id = i + 1  
+            titulo = f"Jogo {jogo_id}"
+            desenvolvedor = f"Desenvolvedor {jogo_id}"
+            preco = round(random.uniform(10, 60))  # Preço aleatório entre 10 e 60
+            generos = random.sample(["Ação", "Aventura", "RPG", "Esportes", "Corrida", "Tiro", "Moba"], random.randint(1, 3))
+
+            jogo = Jogo(jogo_id, titulo, desenvolvedor, preco, generos)
+            jogos[jogo_id] = jogo
+        return jogos
+        
 avltree = AVLtree()
-keys = [10, 40, 60, 20, 30, 100, 10, 20, 30, 6 , 12, 24, 45, 74, 70, 6, 80, 6, 102, 130, 250, 1000]
-for key in keys:
-    avltree.insertNode(key, "value")    
-    
-    
-print(""" 
-      
-      ##########PESQUISA DE JOGOS##########
-      1- ver jogos
-      2- fazer pesquisa por item
-      3- fazer pesquisa por range
-      """)
-userInput = int(input(" "))
-if(userInput == 1):
-    avltree.inOrder()
-    print()
-elif(userInput == 2):
-    price = int(input("insira o preço: "))
-    print(avltree.searchForItem(price))
-elif(userInput == 3):
-    rangeDown = int(input("insira o preço mínimo: "))
-    rangeUp = int(input("insira o preço máximo: "))
-    print(avltree.searchRange(rangeDown, rangeUp))
+keys = [] 
+
+num_jogos = int(input("quantos jogos terão na lista? "))
+jogos_aleatorios = gerar_jogos(num_jogos)
+
+for jogo_id, jogo in jogos_aleatorios.items():
+    avltree.insertNode(jogo.preco, jogo.titulo)
+    print(f"Jogo ID: {jogo.jogo_id}, Título: {jogo.titulo}, Desenvolvedor: {jogo.desenvolvedor}, Preço: {jogo.preco}, Gêneros: {jogo.generos}")
+
+userInput = 0
+while(userInput < 6):
+    print(""" 
+        
+        ##########PESQUISA DE JOGOS##########
+        1- fazer pesquisa por preço
+        2- fazer pesquisa por range de preço
+        3- fazer pesquisa por categoria
+        4- fazer pesquisa por nome
+        5- printar arvore
+        """)
+    userInput = int(input(" "))
+    if(userInput == 1):
+        price = int(input("insira o preço: "))
+        print(avltree.searchForItem(price))
+    elif(userInput == 2):
+        rangeDown = int(input("insira o preço mínimo: "))
+        rangeUp = int(input("insira o preço máximo: "))
+        print(avltree.searchRange(rangeDown, rangeUp))
+    elif(userInput == 3):
+        print("Nao implementado")
+    elif(userInput == 4):
+        print("nao implementado")
+    elif(userInput == 5):
+        avltree.printTree(avltree.base)
+    else:
+        print("invalido")
